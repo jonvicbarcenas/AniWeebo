dce74d846c42eb94f278c67435f9298190132d10
jonvicbarcenas <exchantpro@gmail.com>
2024-10-19 11:35:08 +0800

    chore: Update homepage to show loader while fetching anime data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
-import { useGlobalContext } from '../context/global'
+import React, { useState, useEffect } from 'react';
+import { useGlobalContext } from '../context/global';
 import Card from './Popular-carousel/Card';
 import './Homepage.css';
 import TrendingView from './Popular-carousel/TrendingView';
+import Loader from './screens/Loader';
 
 export default function Homepage() {
     const { popularAnime, trendingAnime } = useGlobalContext();
+    const [loading, setLoading] = useState(true);
+
+    // Add a delay of 2 seconds before showing the anime content
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (popularAnime && trendingAnime) {
+                setLoading(false);
+            }
+        }, 900); 
+
+        // Clean up the timer if the component unmounts
+        return () => clearTimeout(timer);
+    }, [popularAnime, trendingAnime]);
+
     return (
         <>
         <div className="popular-anime">
             <div className="CarouselContainer">
-                <Card anime={popularAnime} className="popular-card" />
-                <TrendingView anime={trendingAnime}/>
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <>
+                        <Card anime={popularAnime} className="popular-card" />
+                        <TrendingView anime={trendingAnime} />
+                    </>
+                )}
             </div>
         </div>
         </>
-    )
+    );
 }
-
-
