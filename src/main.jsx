2b0146ec71a5b52f832033abd30f9fae4fe4674d
jonvicbarcenas <exchantpro@gmail.com>
2024-10-19 15:57:03 +0800

    chore: Update serverURL to use the Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,8 @@ import { AuthContextProvider } from './context/authContext';
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-<<<<<<< HEAD
-<<<<<<< HEAD
-axios.defaults.serverURL = 'http://localhost:5000';
-// axios.defaults.serverURL = 'https://aniweeb-server-db.vercel.app';
-=======
 // axios.defaults.serverURL = 'http://localhost:5000';
 axios.defaults.serverURL = 'https://aniweeb-server-db.vercel.app';
->>>>>>> da9fae7 (idk bai)
-=======
-// axios.defaults.serverURL = 'http://localhost:5000';
-axios.defaults.serverURL = 'https://aniweeb-server-db.vercel.app';
->>>>>>> 57b4a59b8ab99f20159fa4c18cfa7fec9cf4f265
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
